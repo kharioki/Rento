058251071b30c55113a7a8917db9daf0d1bfbe45
kharioki <antolisah@gmail.com>
2020-09-10 09:03:59 +0300

    car model constants

--- lib/screens/available/available_car_screen.dart
+++ lib/screens/available/available_car_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:rento/constants.dart';
+import 'package:rento/model/car.dart';
 
 class AvailableCarScreen extends StatelessWidget {
   @override
@@ -8,7 +9,8 @@ class AvailableCarScreen extends StatelessWidget {
       backgroundColor: mPrimaryColor,
       appBar: buildAppBar(),
       body: ListView.builder(
-        itemBuilder: (context, index) => Container(),
+        itemCount: carList.length,
+        itemBuilder: (context, index) => CarListItem(index),
       ),
     );
   }
@@ -27,6 +29,80 @@ class AvailableCarScreen extends StatelessWidget {
           onPressed: () {},
         )
       ],
+    );
+  }
+}
+
+class CarListItem extends StatelessWidget {
+  const CarListItem(
+    this.index, {
+    Key key,
+  }) : super(key: key);
+
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    Car car = carList[index];
+    return Container(
+      margin: EdgeInsets.only(bottom: 20),
+      child: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            margin: EdgeInsets.only(left: 24, right: 24),
+            padding: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: mCardColor,
+              borderRadius: BorderRadius.circular(16),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '\$${car.price}',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'price/hr',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(height: 16),
+                Row(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          car.brand,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        Text(
+                          'Brand',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
